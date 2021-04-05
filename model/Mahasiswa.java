663d50f92b37ed9c38ae4f62f23ec6c75de8961e
HarfaApriza <Harfaromansyah@gmail.com>
2021-04-05 20:54:39 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("==Data Mahasiswa ================");
+  System.out.println("NPM  : 19630648");
+  System.out.println("Nama : Harfa Apriza");
+  System.out.println("=================================");
+ }
+}
